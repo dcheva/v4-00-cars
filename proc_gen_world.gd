a07b7491062e817458827b9c922012077aa48c63
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-17 17:04:17 +0300

    New tilemap with 4 atlases for gravel ground grass-dry

--- proc_gen_world.gd
+++ proc_gen_world.gd
@@ -7,16 +7,18 @@ var noise: Noise
 var width:  int = 512
 var height: int = 512
 
-var mores: int
-var leses: int
-var zeros: int
-var cente: float = 0.333
+var gravel: int
+var ground: int
+var grassg: int
+var grassd: int
+var cente: float = 0.25
 var noise_array: Array
 
 var source_id = 0
-var gravel_atlas = Vector2i(0, 1)
-var bushes_atlas = Vector2i(0, 1)
-var ground_atlas = Vector2i(0, 1)
+var gravel_atlas = Vector2i( 0, 0)
+var ground_atlas = Vector2i( 4, 0)
+var grassd_atlas = Vector2i( 8, 1)
+var grassg_atlas = Vector2i(12, 1)
 
 func _ready() -> void:
 	noise = noise_height_texture.noise
@@ -28,17 +30,22 @@ func generate_world() -> void:
 		for y in range(height):
 			noise_val = noise.get_noise_2d(x,y)
 			noise_array.append(noise_val)
-			if noise_val < -cente:
-				leses += 1
-				# gravel
-			elif noise_val > cente:
-				mores += 1
-				# bushes
+			if noise_val < 0:
+				if noise_val < -cente:
+					gravel += 1
+					# gravel
+				else:
+					ground += 1
+					# ground
 			else:
-				zeros += 1
-				# ground
+				if noise_val > cente:
+					grassg += 1
+					# grassg
+				else:
+					grassd += 1
+					# grassd
 		
-	var s = "-cente(gravel) : %s\n cente(ground) : %s\n+cente(bushes) : %s\n" % [leses, zeros, mores]
+	var s = "gravel : %s\nground : %s\ngrassd : %s\ngrassg : %s\n" % [gravel, ground, grassg, grassd]
 	print("Min: %s" % noise_array.min())
 	print("Max: %s" % noise_array.max())
 	print("Med: %s" % med(noise_array))
