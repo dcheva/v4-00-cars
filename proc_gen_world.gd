d0cda5c51aee94e63165e29fd1a03658e1a84af7
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-18 03:08:44 +0300

    Add 16 piles with generator

--- proc_gen_world.gd
+++ proc_gen_world.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # @TODO preload settings
 @export var chunk_size: int = 512
-var half_chunk: int = chunk_size / 2
+var half_chunk: int = 256 # chunk_size / 2
 
 @export var noise_height_texture: NoiseTexture2D
 var noise: Noise
@@ -27,14 +27,14 @@ func _ready() -> void:
 	generate_world()
 	
 func generate_world() -> void:
-
+	var piles = 0
 	var noise_val
 	for x in range(-half_chunk, half_chunk):
 		for y in range(-half_chunk, half_chunk):
 			
 			var vpos = Vector2i(posmod(x, atlas_size),posmod(y, atlas_size))
 			
-			noise_val = noise.get_noise_2d(x,y)
+			noise_val = noise.get_noise_2d(x, y)
 			noise_array.append(noise_val)
 			
 			if noise_val < 0:
@@ -55,12 +55,20 @@ func generate_world() -> void:
 					grassd += 1
 					# grassd
 					$TileMapLayer.set_cell(Vector2i(x, y), source_id, grassd_atlas + vpos)
-		
-	var s = "gravel : %s\nground : %s\ngrassd : %s\ngrassg : %s\n" % [gravel, ground, grassg, grassd]
-	print("Min: %s" % noise_array.min())
-	print("Max: %s" % noise_array.max())
-	print("Med: %s" % med(noise_array))
+			
+			# Add statics 
+			var kk = noise_val * 1000
+			if abs(x * 2) < half_chunk and abs(y * 2) < half_chunk: # 64x64 piles on 32x32 TileMap
+				if posmod(kk,  103) > 100: # 2%
+					piles += 1
+					$StaticTileMapLayer.set_cell(Vector2i(x, y), source_id, Vector2i(0, posmod(kk,  17)))
+
+	var s = "gravel : %s\nground : %s\ngrassd : %s\ngrassg : %s" % [gravel, ground, grassg, grassd]
+	print("Min : %s" % noise_array.min())
+	print("Max : %s" % noise_array.max())
+	print("Med : %s" % med(noise_array))
 	print(s)
+	print("+piles : %s" % piles)
 		
 # Math
 func sum(arr:Array):
